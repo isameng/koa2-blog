ff93329ea324ecd49fa3de284cd8c07bfdea670d
ameng <>
2021-01-29 17:19:22 +0800

    feat: 用户设置 单元测试

--- test/user/login.test.js
+++ test/user/login.test.js
@@ -63,11 +63,33 @@ test('登录，应该成功', async () => {
   COOKIE = res.headers['set-cookie'].join(';');
 });
 
+//修改基本信息
+test('修改基本信息，应该成功', async () => {
+  const res = await server.patch('/api/user/changeInfo').send({
+    nickName: '测试昵称', city: '测试城市', picture: '/test.png'
+  }).set('cookie', COOKIE);
+  expect(res.body.errno).toBe(0);
+})
+
+//修改密码
+test('修改密码，应该成功', async () => {
+  const res = await server.patch('/api/user/changePassword').send({
+    password, newPassword:`p_${Date.now()}`
+  }).set('cookie', COOKIE);
+  expect(res.body.errno).toBe(0);
+})
+
 //删除
 test('删除用户，应该成功', async () => {
   const res = await server.post('/api/user/delete').set('cookie', COOKIE);
   expect(res.body.errno).toBe(0);
 });
+
+//退出登录
+test('退出登录，应该成功', async () => {
+  const res = await server.post('/api/user/logout').set('cookie', COOKIE);
+  expect(res.body.errno).toBe(0);
+})
 
 //再次查询用户
 test('删除用户之后，再次查询注册的用户名，应该不存在', async () => {
